c946d00b8b4364ad03126f275789bb1a314f473d
peter88213 <petert88213@gmail.com>
2024-09-08 21:19:53 +0200

    Finish the experimental Python build script.

--- tools/build.py
+++ tools/build.py
@@ -1,4 +1,4 @@
-"""Build the nv_aeon2 novelibre plugin.
+"""Build the nv_aeon2 novelibre plugin package.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the novxlib package.
@@ -11,6 +11,7 @@ For further information see https://github.com/peter88213/nv_aeon2
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
+from shutil import copytree
 from shutil import rmtree
 import sys
 sys.path.insert(0, f'{os.getcwd()}/../../novxlib/src')
@@ -18,27 +19,45 @@ import inliner
 import build_tools
 import translate_de
 
-VERSION = 'x.x.x'
+VERSION = '4.7.1'
 
 PRJ_NAME = 'nv_aeon2'
-RELEASE = f'{PRJ_NAME}_v{VERSION}'
-MO_FILE = 'nv_aeon2.mo'
-
 LOCAL_LIB = 'nvaeon2lib'
-LOCAL_LIB_PATH = '../../nv_aeon2/src/'
+
+VERSION_INI = f'''[LATEST]
+version = {VERSION}
+download_link = https://github.com/peter88213/{PRJ_NAME}/raw/main/dist/{PRJ_NAME}_v{VERSION}.pyzw'''
+VERSION_INI_PATH = '../VERSION'
+LANDING_PAGE = '../README.md'
+LANDING_PAGE_TEMPLATE = '../docs/template/README.md'
+
+RELEASE = f'{PRJ_NAME}_v{VERSION}'
+MO_FILE = f'{PRJ_NAME}.mo'
 SOURCE_DIR = '../src/'
 TEST_DIR = '../test/'
-SOURCE_FILE = f'{SOURCE_DIR}nv_aeon2.py'
-TEST_FILE = f'{TEST_DIR}nv_aeon2.py'
+SOURCE_FILE = f'{SOURCE_DIR}{PRJ_NAME}.py'
+TEST_FILE = f'{TEST_DIR}{PRJ_NAME}.py'
 BUILD_BASE = '../build'
 BUILD_DIR = f'{BUILD_BASE}/{RELEASE}'
 DIST_DIR = '../dist'
+ICON_DIR = f'{SOURCE_DIR}icons'
+SAMPLE_DIR = '../sample'
 
 distFiles = [
     (TEST_FILE, BUILD_DIR),
     (f'{SOURCE_DIR}setuplib.py', BUILD_DIR),
     ('../LICENSE', BUILD_DIR),
 ]
+
+
+def add_icons():
+    print('\nAdding icon files ...')
+    copytree(ICON_DIR, f'{BUILD_DIR}/icons')
+
+
+def add_sample():
+    print('\nAdding sample files ...')
+    copytree(SAMPLE_DIR, f'{BUILD_DIR}/{PRJ_NAME}_sample')
 
 
 def build_package():
@@ -54,15 +73,12 @@ def build_package():
 
 def build_plugin():
     os.makedirs(TEST_DIR, exist_ok=True)
-    inliner.run(SOURCE_FILE, TEST_FILE, LOCAL_LIB, LOCAL_LIB_PATH)
+    inliner.run(SOURCE_FILE, TEST_FILE, LOCAL_LIB, SOURCE_DIR)
     build_tools.inline_modules(TEST_FILE, TEST_FILE)
     build_tools.insert_version_number(TEST_FILE, version=VERSION)
 
 
 def build_translation():
-    if not MO_FILE:
-        return
-
     if not build_tools.make_pot(TEST_FILE, app=PRJ_NAME, version=VERSION):
         sys.exit(1)
 
@@ -95,12 +111,30 @@ def prepare_package():
         )
 
 
+def rewrite_landing_page():
+    print(f'\nRewriting "{LANDING_PAGE}" ...')
+    with open(LANDING_PAGE_TEMPLATE, 'r', encoding='utf_8') as f:
+        text = f.read().replace('0.99.0', VERSION)
+    with open(LANDING_PAGE, 'w', encoding='utf_8', newline='\n') as f:
+        f.write(text)
+
+
+def write_version_ini():
+    print(f'\nRewriting "{VERSION_INI_PATH}" ...')
+    with open(VERSION_INI_PATH, 'w', encoding='utf_8', newline='\n') as f:
+        f.write(VERSION_INI)
+
+
 def main():
     build_plugin()
     build_translation()
     prepare_package()
+    add_sample()
+    add_icons()
     build_package()
     clean_up()
+    write_version_ini()
+    rewrite_landing_page()
 
 
 if __name__ == '__main__':
