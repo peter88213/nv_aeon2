dab7fcd05b9792598c7069374de1cc29ba212bce
peter88213 <petert88213@gmail.com>
2024-11-03 09:05:44 +0100

    Refactor.

--- src/nvaeon2lib/aeon2_converter.py
+++ src/nvaeon2lib/aeon2_converter.py
@@ -11,7 +11,7 @@ from nvlib.novx_globals import Error
 from nvlib.novx_globals import norm_path
 from nvaeon2lib.json_timeline2 import JsonTimeline2
 from nvaeon2lib.nvaeon2_globals import _
-from nvlib.model.nv_service import NvService
+from nvlib.nv_service import NvService
 
 
 class Aeon2Converter(Converter):
