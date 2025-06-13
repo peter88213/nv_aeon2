5d8e245989fbcd678a601f7ca8ec1d4b9ec0c775
peter88213 <petert88213@gmail.com>
2025-06-13 14:54:04 +0200

    Updated docstrings.

--- src/nv_aeon2.py
+++ src/nv_aeon2.py
@@ -72,9 +72,9 @@ class Plugin(PluginBase):
         """Add a submenu to the main menu.
         
         Positional arguments:
-            model -- reference to the main model instance of the application.
-            view -- reference to the main view instance of the application.
-            controller -- reference to the main controller instance of the application.
+            model -- reference to the novelibre main model instance.
+            view -- reference to the novelibre main view instance.
+            controller -- reference to the novelibre main controller instance.
 
         Extends the superclass method.
         """
